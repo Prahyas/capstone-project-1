1afb549c8fe6373242d3470ee545f4b8edfe550f
Aiden Aidenkelly Kelly <aidenkelly@Aidens-MBP.home>
2024-04-08 22:09:19 -0400

    Added addToTotalCost, returnChange, CustomerTest, fixed LogTest, changed to BigDecimal numbers

--- src/main/java/com/techelevator/Customer.java
+++ src/main/java/com/techelevator/Customer.java
@@ -1,50 +1,59 @@
 package com.techelevator;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Customer {
-
-    private static double currentBalance = 0;
-    private static final double quarter = 0.25;
-    private static final double dime = 0.10;
-    private static final double nickel = 0.01;
+    // Customer's current balance, statring with $0
+    private static BigDecimal currentBalance = new BigDecimal("0.00");
+    // Total purchase cost by Customer, starting with $0
+    private static BigDecimal totalCost = new BigDecimal("0.00");
+    // Value of a quarter, dime and nickel coin
+    private static final BigDecimal QUARTER = new BigDecimal("0.25");
+    private static final BigDecimal DIME = new BigDecimal("0.10");
+    private static final BigDecimal NICKEL = new BigDecimal("0.05");
 
     // Return the current balance of the customer
-    public static double getCurrentBalance() {
+    public static BigDecimal getCurrentBalance() {
         return currentBalance;
     }
 
+    // Return the current total cost
+    public static BigDecimal getTotalCost() {
+        return totalCost;
+    }
+
     // Feed money and update the current balance
-    public static void deposit(int amountToDeposit) {
-        currentBalance += amountToDeposit;
-        Log.log(String.format("FEED MONEY: $%d.00 $%.2f", amountToDeposit, currentBalance));
+    public static void deposit(int amount) {
+        BigDecimal amountToDeposit = new BigDecimal(amount);
+        currentBalance = currentBalance.add(amountToDeposit);
+
     }
 
     // Subtract product price from the current balance and update the current balance
-    public static void dispense(double productPrice) {
-        currentBalance -= productPrice;
-        // Need to get productName, productSlotLocation, productPrice here by reading product code input
-        // or, in the Application class and I will implement log method in the application as well
-
-        /* Also, need to print out the following messages
-           All chip items print "Crunch Crunch, Yum!"
-           All candy items print "Munch Munch, Yum!"
-           All drink items print "Glug Glug, Yum!"
-           All gum items print "Chew Chew, Yum!"
-         */
-
+    public static void purchase(BigDecimal itemPrice) {
+        currentBalance = currentBalance.subtract(itemPrice);
         //Log.log(String.format("productName productLocation: $%.2f $%.2f", productPrice, currentBalance));
     }
 
     // After finishing transaction, run this method to make current balance to zero and record in the log file
     public static void zeroBalance() {
-        Log.log(String.format("GIVE CHANGE: $%.2f $%.2f", currentBalance, 0.00));
-        currentBalance = 0;
+        currentBalance = currentBalance.subtract(currentBalance);
     }
 
     // Return the customer's money using quarters, dimes and nickels in the smallest amount of coins possible
     public static void returnChange() {
-
+        int numberOfQuarters = (currentBalance.divide(QUARTER)).intValue();
+        int numberOfDimes = ((currentBalance.subtract(QUARTER.multiply(new BigDecimal(numberOfQuarters)))).divide(DIME)).intValue();
+        int numberOfNickels = ((currentBalance.subtract((QUARTER.multiply(new BigDecimal(numberOfQuarters))).add(DIME.multiply(new BigDecimal(numberOfDimes))))).divide(NICKEL)).intValue();
+        int numberOfPenny = (currentBalance.subtract((QUARTER.multiply(new BigDecimal(numberOfQuarters))).add(DIME.multiply(new BigDecimal(numberOfDimes))).add(NICKEL.multiply(new BigDecimal(numberOfNickels))))).intValue();
+        System.out.println(
+                String.format("Returned customer %d quarter(s), %d dime(s), %d nickel(s) and %d penny(ies)",
+                        numberOfQuarters, numberOfDimes, numberOfNickels, numberOfPenny));
     }
 
+    // When dispensing an item, add the purchased product price to the current total cost
+    public static void addToTotalCost(BigDecimal itemPrice) {
+        totalCost = totalCost.add(itemPrice);
+    }
 }
--- src/test/java/com/techelevator/CustomerTest.java
+++ src/test/java/com/techelevator/CustomerTest.java
@@ -1,4 +1,54 @@
 package com.techelevator;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
 public class CustomerTest {
+
+    private BigDecimal currentBalance = new BigDecimal("0.00");
+    private BigDecimal feedMoneyValue = new BigDecimal("5.00");
+    private BigDecimal itemPrice = new BigDecimal("4.15");
+
+    @Before
+    public void makeCurrentBalanceToZero() {
+        Customer.zeroBalance();
+    }
+
+    @Test
+    public void checkDeposit() {
+        Customer.deposit(feedMoneyValue.intValue());
+        System.out.println("Expected: " + currentBalance.add(feedMoneyValue));
+        System.out.println("Actual: " + Customer.getCurrentBalance());
+        Assert.assertEquals(currentBalance.add(feedMoneyValue), Customer.getCurrentBalance());
+        Customer.zeroBalance();
+    }
+
+    @Test
+    public void checkPurchase() {
+        Customer.purchase(itemPrice);
+        System.out.println("Expected: " + currentBalance.subtract(itemPrice));
+        System.out.println("Actual: " + Customer.getCurrentBalance());
+        Assert.assertEquals(currentBalance.subtract(itemPrice), Customer.getCurrentBalance());
+    }
+
+    @Test
+    public void checkReturnChange() {
+        Customer.deposit(feedMoneyValue.intValue());
+        Customer.purchase(itemPrice); // current balance = 5.00 - 4.15 = 0.85 -> 3 quarters and 1 dime
+        System.out.println("Expected: Returned customer 3 quarter(s), 1 dime(s), 0 nickel(s) and 0 penny(ies)");
+        System.out.print("Actual: ");
+        Customer.returnChange();
+    }
+
+    @Test
+    public void zerorizeBalance() {
+        Customer.deposit(feedMoneyValue.intValue());
+        Customer.zeroBalance();
+        Assert.assertEquals(new BigDecimal("0.00"), Customer.getCurrentBalance());
+    }
 }
+
